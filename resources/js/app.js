c63ee2a14025294aac2cbeb9c2734508ed41636f
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2024-12-21 14:46:17 +0300

    PrimeVue added

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,20 +5,36 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => 'PrimeVue + Inertia',
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
             import.meta.glob('./Pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
+        document.documentElement.classList.add('app-dark');
+        
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura,
+                    options: {
+                        darkModeSelector: '.app-dark'
+                    }
+                }
+            })
+            .component('Button', Button)
+            .component('InputText', InputText)
             .mount(el);
     },
     progress: {
